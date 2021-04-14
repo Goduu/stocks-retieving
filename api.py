e2f92255cf7c93d73882163168f1b1b7f5c5eab5
goduu <idcangussu@hotmai.com>
2021-04-14 08:12:21 -0300

    modified:   api.py

--- api.py
+++ api.py
@@ -146,7 +146,8 @@ def get_earnings_history():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000", debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
     # socketio.run(app,port=5000, host='0.0.0.0')
 
 # if __name__ == "__main__":
